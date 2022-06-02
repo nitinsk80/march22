c4775e9fd19143caf53964d1366e515fe2ccb966
nitinsk80 <nitinsk80@gmail.com>
2022-06-02 23:28:28 +0530

    demo test

---
+++ JAVA/src/JavaTest/Test8.java
@@ -0,0 +1,8 @@
+package JavaTest;
+
+public class Test8 {
+	public static void main(String[]args) {
+		System.out.println("....Hello....");
+
+}
+}
---
+++ JAVA/src/JavaTest/Test9.java
@@ -0,0 +1,8 @@
+package JavaTest;
+
+public class Test9 {
+	public static void main(String[]args) {
+		System.out.println("....Hello....");
+
+}
+}
